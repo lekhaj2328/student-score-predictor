53f361cba4c335c4ca32857dccbcce68e35b756e
lekhaj2328 <lekhaj222@gmail.com>
2026-05-17 12:21:58 +0530

    Updated app for deployment

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -18,4 +19,5 @@ def home():
     return render_template("index.html", prediction=prediction)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
